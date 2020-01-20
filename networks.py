222bdf3a270b33040d70e4ec812af9fb269f5531
Henry Prior <hp999@nyu.edu>
2020-01-20 18:55:33 -0500

    networks final

--- networks.py
+++ networks.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
+import numpy as np
 
-class CategoricalSample(tf.keras.Model):
+class CategoricalSampler(tf.keras.Model):
   def call(self, logits):
     return tf.squeeze(tf.random.categorical(logits, 1), axis=-1)
 
@@ -9,8 +10,8 @@ class Actor(tf.keras.Model):
                  action_space_size: int,
                  name: str = None):
         super(Actor, self).__init__(name=name)
-        self.hidden_1 = tf.keras.layers.Dense(64, activation='relu')
-        self.hidden_2 = tf.keras.layers.Dense(64, activation='relu')
+        self.hidden_1 = tf.keras.layers.Dense(128, activation='relu')
+        self.hidden_2 = tf.keras.layers.Dense(128, activation='relu')
 
         self.logits = tf.keras.layers.Dense(action_space_size)
 
@@ -24,8 +25,8 @@ class Actor(tf.keras.Model):
 class Critic(tf.keras.Sequential):
     def __init__(self,
                  name: str = None):
-        hidden_1 = tf.keras.layers.Dense(64, activation='relu')
-        hidden_2 = tf.keras.layers.Dense(64, activation='relu')
+        hidden_1 = tf.keras.layers.Dense(128, activation='relu')
+        hidden_2 = tf.keras.layers.Dense(128, activation='relu')
         state_out = tf.keras.layers.Dense(1)
         super(Critic, self).__init__([hidden_1, hidden_2, state_out], name)
 
@@ -33,7 +34,10 @@ class Critic(tf.keras.Sequential):
 class ActorCriticModel(tf.keras.Model):
     def __init__(self,
                  actor_network: tf.keras.Model,
-                 critic_network: tf.keras.Model):
+                 critic_network: tf.keras.Model,
+                 name: str = None):
+        super(ActorCriticModel, self).__init__(name=name)
+
         self.actor_network = actor_network
         self.critic_network = critic_network
         self.sampler = CategoricalSampler()
@@ -47,7 +51,7 @@ class ActorCriticModel(tf.keras.Model):
         # Action selection needs to be outside of the call() function because we don't do backprop through it
         # predict_on_batch is faster than call in TF 2.0
         logits, value = self.predict_on_batch(inputs)
-        action = self.sampler.predict_on_batch(inputs)
+        action = self.sampler.predict_on_batch(logits)
 
-        return action, value
+        return np.squeeze(action), np.squeeze(value)
 
